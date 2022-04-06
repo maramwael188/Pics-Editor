4c16c4a1eecf0e8b805e3e6d332f6d031dd3394e
maramwael188 <100848741+maramwael188@users.noreply.github.com>
2022-04-06 18:54:57 +0200

    added pics-editor

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+using namespace std;
+int half_size {SIZE / 2};
+
+unsigned char image [SIZE][SIZE];
+void loadImage(){
+    char imageFileName[100];
+    cout << "Enter the source image file name: ";
+    cin >> imageFileName;
+    strcat (imageFileName, ".bmp");
+    readGSBMP (imageFileName, image);
+}
+void saveImage(){
+    char imageFileName[100];
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+    strcat (imageFileName, ".bmp");
+    writeGSBMP (imageFileName, image);
+}
+
+int main() {
+    while (true) {
+        cout << "What do you like to do: " << endl;
+        cout << "1-Black and white image." << endl;
+        cout << "2-Invert image." << endl;
+        cout << "3-Merge Images." << endl;
+        cout << "4-Flip Image." << endl;
+        cout << "5-Rotate Image." << endl;
+        cout << "6-Darken and Lighten Image." << endl;
+        cout << "7-Detect Image Edges." << endl;
+        cout << "8-Enlarge Image." << endl;
+        cout << "9-Shrink Image." << endl;
+        cout << "a-Mirror Image." << endl;
+        cout << "b-Shuffle Image." << endl;
+        cout << "c-Blur Image." << endl;
+        cout << "0-Exit." << endl;
+        cout << "enter number or char: ";
+        char option;
+        cin >> option;
+        if (option == '0') {
+            break;
+        }
+        loadImage();
+        switch (option) {
+            case '1':
+
+                break;
+            case '2':
+
+                break;
+            case '3':
+
+                break;
+            case '4':
+
+                break;
+            case '5':
+
+                break;
+            case '6':
+
+                break;
+            case '7':
+
+                break;
+            case '8':
+
+                break;
+            case '9':
+
+                break;
+            case 'a':
+
+                break;
+            case 'b':
+
+                break;
+            case 'c':
+
+                break;
+        }
+        saveImage();
+    }
+}
